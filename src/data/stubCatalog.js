0427322d9c34fe335bc1610c77f4cd2faa6892b6
dekciw <kykypy3ka536@gmail.com>
2026-03-31 20:38:28 +0300

    feat: добавлен каталог-заглушка STUB_CATALOG (серии, модели, замки, наценки)

---
+++ src/data/stubCatalog.js
@@ -0,0 +1,69 @@
+export const STUB_CATALOG = {
+  series: [
+    { id: 'ml', name: 'Серия «ML»' },
+    { id: 'sl', name: 'Серия «SL»' },
+  ],
+
+  models: {
+    'ml-usi': {
+      name: 'Шкаф металлический усиленный',
+      seriesId: 'ml',
+      basePrice: 12000,
+      article: 'SHL-ML-USI',
+      defaultSpecs: {
+        width: 400,
+        height: 1850,
+        thickness: '0.5',
+        lockId: 'key_basic',
+        ventilation: false,
+        bodyColorName: 'RAL 7038',
+        doorColorName: 'RAL 7038',
+      },
+    },
+    'ml-std': {
+      name: 'Шкаф металлический стандартный',
+      seriesId: 'ml',
+      basePrice: 9500,
+      article: 'SHL-ML-STD',
+      defaultSpecs: {
+        width: 400,
+        height: 1800,
+        thickness: '0.5',
+        lockId: 'key_basic',
+        ventilation: false,
+        bodyColorName: 'RAL 7038',
+        doorColorName: 'RAL 7038',
+      },
+    },
+    'sl-lite': {
+      name: 'Шкаф металлический lite',
+      seriesId: 'sl',
+      basePrice: 7200,
+      article: 'SHL-SL-LITE',
+      defaultSpecs: {
+        width: 300,
+        height: 1800,
+        thickness: '0.5',
+        lockId: 'key_basic',
+        ventilation: false,
+        bodyColorName: 'RAL 7038',
+        doorColorName: 'RAL 7038',
+      },
+    },
+  },
+
+  locks: {
+    'key_basic': { name: 'Ключевой (Базовый)', surcharge: 0 },
+    'lock_2':    { name: 'Замок навесной', surcharge: 800 },
+    'lock_3':    { name: 'Замок кодовый', surcharge: 1200 },
+    'lock_4':    { name: 'Замок сувальдный', surcharge: 1500 },
+    'lock_5':    { name: 'Замок электронный', surcharge: 2100 },
+  },
+
+  thicknessSurcharges: {
+    '0.6': 800,
+    '0.7': 1500,
+  },
+
+  ventSurcharge: 500,
+};
