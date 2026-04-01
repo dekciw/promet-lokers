21ae89525fdf5ddc652d9c1ddc30c96149b46b3f
dekciw <kykypy3ka536@gmail.com>
2026-04-01 09:37:38 +0300

    Обновил хедер: убрал лого Промет, поменял фон на #0C53B3, расширил города вправо

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,6 @@ export default function Header() {
 	return (
 		<header className='header'>
 			<div className='brand'>
-				<img className='logo' src='/img/logo.svg' alt='Промет' />
 				<div className='info'>
 					<span className='site-name'>safe.ru</span>
 					<span className='tagline'>Калькулятор коррекции цен и передачи локеров</span>
