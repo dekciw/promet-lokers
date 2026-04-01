6cb416791c66abf0cd6ff0f8ff80805d9a4066d8
dekciw <kykypy3ka536@gmail.com>
2026-04-01 09:44:16 +0300

    Вернул логотип Промет в левую часть хедера

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,6 +4,7 @@ export default function Header() {
 	return (
 		<header className='header'>
 			<div className='brand'>
+				<img className='logo' src='/img/logo.svg' alt='Промет' />
 				<div className='info'>
 					<span className='site-name'>safe.ru</span>
 					<span className='tagline'>Калькулятор коррекции цен и передачи локеров</span>
